c0c751d014bcef61ee55eff40c5ec706efc3d9d0
Rashmika Fernando <shwemelk2004@gmail.com>
2024-12-13 22:47:51 +0530

    NewVersion-checkoutservlet.java

---
+++ Back-end/checkoutservlet.java
@@ -0,0 +1,32 @@
+package checkout;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+
+public class CheckoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int movieId = Integer.parseInt(request.getParameter("movie_id"));
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/abc_cinema", "root", "123456");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM movies WHERE movie_id = ?");
+            ps.setInt(1, movieId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                request.setAttribute("movie_id", rs.getInt("movie_id"));
+                request.setAttribute("movie_name", rs.getString("movie_name"));
+                request.setAttribute("description", rs.getString("description"));
+                request.setAttribute("genre", rs.getString("genre"));
+                request.setAttribute("price", rs.getDouble("price"));
+                request.setAttribute("image_url", rs.getString("image_url"));
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("checkout.jsp");
+        rd.forward(request, response);
+    }
+}
