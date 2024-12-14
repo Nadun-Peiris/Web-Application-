f02ed6438f034ae8178700050788d9b22471ec69
Adithya-Dewmini <dewminia18@gmail.com>
2024-12-14 13:08:18 +0530

    LoginServlet.java

---
+++ Back-end/LoginServlet.java
@@ -0,0 +1,57 @@
+package User;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import Connection.dbcon;
+@WebServlet(name = "LoginServlet", value = "/loginServlet")
+public class LoginServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = dbcon.getConnection();
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                HttpSession session = req.getSession();
+                session.setAttribute("username", username);
+                resp.sendRedirect("home.jsp");
+            } else {
+                resp.sendRedirect("signup.jsp");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) dbcon.closeConnection(connection);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
